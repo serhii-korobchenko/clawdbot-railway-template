247a5efc1025feac9df9c342b2df22f2f8ab60ca
Serhii Korobchenko <105776433+serhii-korobchenko@users.noreply.github.com>
2026-09-18 10:23:15 +0300

    Run PROROK refresh collector v7

--- src/start-with-prorok-api.js
+++ src/start-with-prorok-api.js
@@ -90,7 +90,7 @@ spawnManaged(
 spawnManaged(
   "python3",
   [
-    "prorok/prorok_refresh_collector_v6.py",
+    "prorok/prorok_refresh_collector_v7.py",
     "--interval-seconds",
     collectorInterval,
   ],
